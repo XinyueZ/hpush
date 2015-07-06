5b79f8899345f527214f3b7a76449b8ff1c6469c
XinyueZhao <zhao@eosnewmedia.de>
2015-07-06 21:10:15 +0200

    Update backend for topic-messages.

--- push/broadcast.go
+++ push/broadcast.go
@@ -9,9 +9,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strings"
-	"time"
 	"strconv"
+	"time"
 )
 
 type TopStoresRes struct {
@@ -47,14 +46,16 @@ type SyncItemDetailsList struct {
 	SyncList []SyncItemDetails
 }
 
-func getTopStories(_w http.ResponseWriter, _r *http.Request) []int64 {
-	cxt := appengine.NewContext(_r)
-	if req, err := http.NewRequest(API_METHOD, API_GET_TOP_STORIES, nil); err == nil {
+//Get list of all "ids" of items.
+func getTopStories(w http.ResponseWriter, r *http.Request, api string) []int64 {
+	apiUrl := API_HOST + API_VERSION + "/" + api + ".json"
+	cxt := appengine.NewContext(r)
+	if req, err := http.NewRequest(API_METHOD, apiUrl, nil); err == nil {
 		httpClient := urlfetch.Client(cxt)
-		_r, err := httpClient.Do(req)
-		defer _r.Body.Close()
+		r, err := httpClient.Do(req)
+		defer r.Body.Close()
 		if err == nil {
-			if bytes, err := ioutil.ReadAll(_r.Body); err == nil {
+			if bytes, err := ioutil.ReadAll(r.Body); err == nil {
 				topStoresRes := make([]int64, 0)
 				json.Unmarshal(bytes, &topStoresRes)
 				return topStoresRes
@@ -70,9 +71,10 @@ func getTopStories(_w http.ResponseWriter, _r *http.Request) []int64 {
 	return nil
 }
 
-func loadDetailsList(_w http.ResponseWriter, _r *http.Request, itemId int64, detailsList *[]*ItemDetails, ch chan int) {
+//Load detail of item.
+func loadItemDetail(w http.ResponseWriter, r *http.Request, itemId int64, detailsList *[]*ItemDetails, ch chan int) {
 	api := fmt.Sprintf(API_GET_ITEM_DETAILS, itemId)
-	cxt := appengine.NewContext(_r)
+	cxt := appengine.NewContext(r)
 	if req, err := http.NewRequest(API_METHOD, api, nil); err == nil {
 		httpClient := urlfetch.Client(cxt)
 		res, err := httpClient.Do(req)
@@ -86,25 +88,26 @@ func loadDetailsList(_w http.ResponseWriter, _r *http.Request, itemId int64, det
 				*detailsList = append(*detailsList, details)
 				ch <- 0
 			} else {
-				cxt.Errorf("loadDetailsList unmarshal: %v", err)
+				cxt.Errorf("loadItemDetail unmarshal: %v", err)
 				ch <- 0
 			}
 		} else {
-			cxt.Errorf("loadDetailsList doing: %v", err)
+			cxt.Errorf("loadItemDetail doing: %v", err)
 			ch <- 0
 		}
 	} else {
-		cxt.Errorf("loadDetailsList: %v", err)
+		cxt.Errorf("loadItemDetail: %v", err)
 		ch <- 0
 	}
 }
 
-func getItemDetails(_w http.ResponseWriter, _r *http.Request, itemIds []int64) []*ItemDetails {
+//Get object detail.
+func getItemDetails(w http.ResponseWriter, r *http.Request, itemIds []int64) []*ItemDetails {
 	detailsList := []*ItemDetails{}
 	ch := make(chan int)
 
 	for _, itemId := range itemIds {
-		go loadDetailsList(_w, _r, itemId, &detailsList, ch)
+		go loadItemDetail(w, r, itemId, &detailsList, ch)
 	}
 
 	c := len(itemIds)
@@ -115,125 +118,80 @@ func getItemDetails(_w http.ResponseWriter, _r *http.Request, itemIds []int64) [
 	return detailsList
 }
 
-func dispatch(_w http.ResponseWriter, _r *http.Request, roundTotal *int,
-	client OtherClient, itemDetail *ItemDetails, pushedTime string, scheduledTask bool, ch chan int) (brk bool) {
-	brk = false
-	if ch != nil {
-		if *roundTotal < client.MsgCount {
-			if client.FullText && len(strings.TrimSpace(itemDetail.Text)) == 0 {
-				ch <- 0
-			} else if !client.AllowEmptyUrl && len(strings.TrimSpace(itemDetail.Url)) == 0 {
-				ch <- 0
-			} else {
-				if itemDetail != nil {
-					(*roundTotal)++
-					broadcast(_w, _r, client.PushID, itemDetail, pushedTime, scheduledTask)
+//Dispatch messages to client, do details of push.
+func dispatchOnClients(w http.ResponseWriter, r *http.Request, topicApi string, pItemDetailsList *[]*ItemDetails, pushedTime string) {
+	//Dispatch details to client.
+	if pItemDetailsList != nil {
+		c := len(*pItemDetailsList)
+		ch := make(chan int, c)
+		for _, pItemDetail := range *pItemDetailsList {
+			if pItemDetail != nil {
+				go func(w http.ResponseWriter, r *http.Request, topicApi string, pItemDetail *ItemDetails, pushedTime string, ch chan int) {
+					broadcast(w, r, topicApi, pItemDetail, pushedTime)
 					ch <- 0
-				} else {
-					ch <- 0
-				}
+				}(w, r, topicApi, pItemDetail, pushedTime, ch)
 			}
-		} else {
-			ch <- 0
 		}
-	} else {
-		if *roundTotal < client.MsgCount {
-			if client.FullText && len(strings.TrimSpace(itemDetail.Text)) == 0 {
-			} else if !client.AllowEmptyUrl && len(strings.TrimSpace(itemDetail.Url)) == 0 {
-			} else {
-				if itemDetail != nil {
-					(*roundTotal)++
-					broadcast(_w, _r, client.PushID, itemDetail, pushedTime, scheduledTask)
-				}
-			}
-		} else {
-			brk = true
-		}
-	}
-	return
-}
-
-func dispatchOnClients(_w http.ResponseWriter, _r *http.Request, _itemDetailsList []*ItemDetails, client OtherClient, pushedTime string, scheduledTask bool,   syncType bool) {
-	var roundTotal int = 0
-	if !syncType {
-		ch := make(chan int)
-		for _, itemDetail := range _itemDetailsList {
-			go dispatch(_w, _r, &roundTotal, client, itemDetail, pushedTime, scheduledTask, ch)
-		}
-		c := len(_itemDetailsList)
 		for i := 0; i < c; i++ {
 			<-ch
 		}
-	} else {
-		for _, itemDetail := range _itemDetailsList {
-			brk := dispatch(_w, _r, &roundTotal, client, itemDetail, pushedTime, scheduledTask, nil)
-			if brk {
-				break
-			}
-		}
 	}
-
-	if !syncType {
-		endCh := make(chan int)
-		go summary(_w, _r, client, _itemDetailsList, pushedTime, scheduledTask, endCh)
-		<-endCh
-	} else {
-		summary(_w, _r, client, _itemDetailsList, pushedTime, scheduledTask, nil)
-	}
+	//Push summary to client.
+	endCh := make(chan int)
+	go summary(w, r, pItemDetailsList, pushedTime, endCh)
+	<-endCh
 }
 
-func push(_w http.ResponseWriter, _r *http.Request, _clients []OtherClient, scheduledTask bool) {
-	if _clients != nil {
-		_itemDetailsList := getItemDetails(_w, _r, getTopStories(_w, _r))
-		t := time.Now()
-		pushedTime := t.Format("20060102150405")
+//Push messages to clients.
+func push(w http.ResponseWriter, r *http.Request, api string) {
+	topicApi := (TOPICS + api)
+	itemDetailsList := getItemDetails(w, r, getTopStories(w, r, api))
+	t := time.Now()
+	pushedTime := t.Format("20060102150405")
 
-		//Check how many clients needs PUSH.
-		totalDispatch := len(_clients)
-		dispatchCh := make(chan int, totalDispatch)
+	dispatchCh := make(chan int)
 
-		//Dispatch PUSHs to clients.
-		for _, client := range _clients {
-			go func(w http.ResponseWriter, r *http.Request, itemDetailsList []*ItemDetails, client OtherClient, pushedTime string, scheduledTask bool,  syncType bool, dispatchCh chan int) {
-				dispatchOnClients(w, r, itemDetailsList, client, pushedTime, scheduledTask, syncType)
-				dispatchCh <- 0
-			}(_w, _r, _itemDetailsList, client, pushedTime, scheduledTask, true, dispatchCh)
-		}
+	go func(w http.ResponseWriter,
+		r *http.Request,
+		topicApi string,
+		pItemDetailsList *[]*ItemDetails,
+		pushedTime string,
+		dispatchCh chan int) {
 
-		//Wait for all pushed clients.
-		for i := 0; i < totalDispatch; i++ {
-			<-dispatchCh
-		}
-	}
+		dispatchOnClients(w, r, topicApi, pItemDetailsList, pushedTime)
+		dispatchCh <- 0
+	}(w, r, topicApi, &itemDetailsList, pushedTime, dispatchCh)
+
+	<-dispatchCh
 }
 
-func summary(_w http.ResponseWriter, _r *http.Request, _client OtherClient, pushedDetailList []*ItemDetails, pushedTime string, scheduledTask bool, endCh chan int) {
+//Send summary to clients.
+func summary(w http.ResponseWriter, r *http.Request, pItemDetailsList *[]*ItemDetails, pushedTime string, endCh chan int) {
 	//Push server can not accept to long contents.
-	loopCount := len(pushedDetailList)
+	loopCount := len(*pItemDetailsList)
 	if loopCount > SUMMARY_MAX {
 		loopCount = SUMMARY_MAX
 	}
 	msgIds := ""
 	summary := ""
 	for i := 0; i < loopCount; i++ {
-		summary += (pushedDetailList[i].Title + "<tr>")
-		msgIds += (strconv.FormatInt(pushedDetailList[i].Id, 10) + ",")
+		summary += (((*pItemDetailsList)[i]).Title + "<tr>")
+		msgIds += (strconv.FormatInt(((*pItemDetailsList)[i]).Id, 10) + ",")
 	}
-	pushedMsg := fmt.Sprintf(`{"registration_ids" : ["%s"],"data" : {"isSummary" : true, "summary": "%s", "ids": "%s", "count": %d, "pushed_time" : "%s"}}`,
-		_client.PushID,
+	pushedMsg := fmt.Sprintf(`{"to" : "%s", "data" : {"isSummary" : true, "summary": "%s", "ids": "%s", "count": %d, "pushed_time" : "%s"}}`,
+		TOPICS + GET_SUMMARY,
 		summary,
 		msgIds,
-		_client.MsgCount,
+		loopCount,
 		pushedTime)
 	pushedMsgBytes := bytes.NewBufferString(pushedMsg)
 
-	cxt := appengine.NewContext(_r)
+	cxt := appengine.NewContext(r)
 	if req, err := http.NewRequest("POST", PUSH_SENDER, pushedMsgBytes); err == nil {
 		req.Header.Add("Authorization", PUSH_KEY)
 		req.Header.Add("Content-Type", API_RESTYPE)
-		if scheduledTask {
-			req.Header.Add("X-AppEngine-Cron", "true")
-		}
+		req.Header.Add("X-AppEngine-Cron", "true")
+
 		client := urlfetch.Client(cxt)
 		res, _ := client.Do(req)
 		if res != nil {
@@ -250,28 +208,27 @@ func summary(_w http.ResponseWriter, _r *http.Request, _client OtherClient, push
 	}
 }
 
-func broadcast(_w http.ResponseWriter, _r *http.Request, clientIds string, details *ItemDetails, pushedTime string, scheduledTask bool) {
+func broadcast(w http.ResponseWriter, r *http.Request, topics string, pDetails *ItemDetails, pushedTime string) {
 	pushedMsg := fmt.Sprintf(
-		`{"registration_ids" : ["%s"],"data" : {"by": "%s", "c_id": %d, "score": %d, "comments_count": %d, "text": "%s", "time": %d, "title": "%s", "url": "%s", "pushed_time" : "%s"}}`,
-		clientIds,
-		details.By,
-		details.Id,
-		details.Score,
-		len(details.Kids),
+		`{"to" : "%s","data" : {"by": "%s", "c_id": %d, "score": %d, "comments_count": %d, "text": "%s", "time": %d, "title": "%s", "url": "%s", "pushed_time" : "%s"}}`,
+		topics,
+		pDetails.By,
+		pDetails.Id,
+		pDetails.Score,
+		len(pDetails.Kids),
 		"", //details.Text,
-		details.Time,
-		details.Title,
-		details.Url,
+		pDetails.Time,
+		pDetails.Title,
+		pDetails.Url,
 		pushedTime)
 	pushedMsgBytes := bytes.NewBufferString(pushedMsg)
 
-	cxt := appengine.NewContext(_r)
+	cxt := appengine.NewContext(r)
 	if req, err := http.NewRequest("POST", PUSH_SENDER, pushedMsgBytes); err == nil {
 		req.Header.Add("Authorization", PUSH_KEY)
 		req.Header.Add("Content-Type", API_RESTYPE)
-		if scheduledTask {
-			req.Header.Add("X-AppEngine-Cron", "true")
-		}
+		req.Header.Add("X-AppEngine-Cron", "true")
+
 		client := urlfetch.Client(cxt)
 		res, err := client.Do(req)
 		if res != nil {
@@ -285,70 +242,33 @@ func broadcast(_w http.ResponseWriter, _r *http.Request, clientIds string, detai
 	}
 }
 
-func sync(_w http.ResponseWriter, _r *http.Request, client *OtherClient) {
-	cxt := appengine.NewContext(_r)
-	_itemDetailsList := getItemDetails(_w, _r, getTopStories(_w, _r))
+//Sync top-news to client.
+func sync(w http.ResponseWriter, r *http.Request) {
+	cxt := appengine.NewContext(r)
+	itemDetailsList := getItemDetails(w, r, getTopStories(w, r, GET_TOP_STORIES))
 	syncItems := []SyncItemDetails{}
-	var roundTotal int = 0
 	t := time.Now()
 	pushedTime := t.Format("20060102150405")
-	for _, itemDetail := range _itemDetailsList {
+	for _, itemDetail := range itemDetailsList {
 		//Same logical like dispatch, but I don't use routine.
-		if roundTotal < client.MsgCount {
-			if client.FullText && len(strings.TrimSpace(itemDetail.Text)) == 0 {
-			} else if !client.AllowEmptyUrl && len(strings.TrimSpace(itemDetail.Url)) == 0 {
-			} else {
-				roundTotal++
-				syncItem := SyncItemDetails{
-					By:          itemDetail.By,
-					Id:          itemDetail.Id,
-					Kids:        len(itemDetail.Kids),
-					Score:       itemDetail.Score,
-					Text:        "", //itemDetail.Text,
-					Time:        itemDetail.Time,
-					Title:       itemDetail.Title,
-					Url:         itemDetail.Url,
-					Pushed_Time: pushedTime}
-				syncItems = append(syncItems, syncItem)
-			}
-		} else {
-			break
-		}
+		syncItem := SyncItemDetails{
+			By:          itemDetail.By,
+			Id:          itemDetail.Id,
+			Kids:        len(itemDetail.Kids),
+			Score:       itemDetail.Score,
+			Text:        "", //itemDetail.Text,
+			Time:        itemDetail.Time,
+			Title:       itemDetail.Title,
+			Url:         itemDetail.Url,
+			Pushed_Time: pushedTime}
+		syncItems = append(syncItems, syncItem)
 	}
 	if len(syncItems) > 0 {
 		if json, err := json.Marshal(SyncItemDetailsList{syncItems}); err == nil {
-			_w.Header().Set("Content-Type", API_RESTYPE)
-			fmt.Fprintf(_w, string(json))
+			w.Header().Set("Content-Type", API_RESTYPE)
+			fmt.Fprintf(w, string(json))
 		} else {
 			cxt.Errorf("sync marshal: %v", err)
 		}
 	}
-}
-
-//Deprecated, in case that we need more quota safe.
-func getTinyUrl(_w http.ResponseWriter, _r *http.Request, orignalUrl string) (tingUrl string) {
-	if orignalUrl != "" {
-		cxt := appengine.NewContext(_r)
-		if req, err := http.NewRequest(API_METHOD, TINY+orignalUrl, nil); err == nil {
-			httpClient := urlfetch.Client(cxt)
-			res, err := httpClient.Do(req)
-			if res != nil {
-				defer res.Body.Close()
-			}
-			if err == nil {
-				if bytes, err := ioutil.ReadAll(res.Body); err == nil {
-					tingUrl = string(bytes)
-				} else {
-					cxt.Errorf("getTinyUrl read: %v", err)
-					tingUrl = orignalUrl
-				}
-			} else {
-				cxt.Errorf("getTinyUrl doing: %v", err)
-				tingUrl = orignalUrl
-			}
-		} else {
-			cxt.Errorf("getTinyUrl: %v", err)
-		}
-	}
-	return
 }
--- push/index.go
+++ push/index.go
@@ -2,214 +2,64 @@ package pushmessager
 
 import (
 	"appengine"
-	"appengine/datastore"
 
 	"fmt"
-	"html/template"
 	"net/http"
-	"strconv"
 )
-
-var indexTemplate = template.Must(template.New("index").Parse(pushForm))
-var responseTemplate = template.Must(template.New("response").Parse(responseForm))
-
-const (
-	pushForm = `
-          <html>
-              <body>
-                </p>
-                <p>Project hpush push-messages.</p></p>
-                <form action="/response" method="post">
-                  <div><input type="submit" value="PUSH" /></div>
-                </form>
-
-                <form action="/dela" method="post">
-                  <div><input type="submit" value="REMOVE ALL USERS"/></div>
-                </form>
-
-            </body>
-          </html>
-        `
-	responseForm = `<html><body><form action="/" method="post">{{.}}</p><div><input type="submit" value="Repush"></div></form></body></html>`
-)
-
-type OtherClient struct {
-	Account       string
-	PushID        string
-	FullText      bool
-	MsgCount      int
-	AllowEmptyUrl bool
-}
 
 func init() {
-	http.HandleFunc("/", handleRoot)
-	http.HandleFunc("/insert", handleInsert)
-	http.HandleFunc("/del", handleDelete)
-	http.HandleFunc("/response", handleResponse)
-	http.HandleFunc("/responseX", handleResponseX) //For scheduled tasks.
-	http.HandleFunc("/dela", handleDeleteAllUsers)
-	http.HandleFunc("/edit", handleEdit)
-	http.HandleFunc("/sync", handleSync)
+	http.HandleFunc("/topstories", handleTopstories)
+	http.HandleFunc("/newstories", handleNewstories)
+	http.HandleFunc("/askstories", handleAskstories)
+	http.HandleFunc("/showstories", handleShowstories)
+	http.HandleFunc("/jobstories", handleJobstories)
 }
 
-/*
-func login(_w http.ResponseWriter, _r *http.Request) {
-  c := appengine.NewContext(_r)
-  u := user.Current(c)
-  if u == nil {
-    url, err := user.LoginURL(c, _r.URL.String())
-    if err != nil {
-      http.Error(_w, err.Error(), http.StatusInternalServerError)
-      return
-    }
-    _w.Header().Set("Location", url)
-    _w.WriteHeader(http.StatusFound)
-  }
-}
-*/
-func handleRoot(_w http.ResponseWriter, _r *http.Request) {
-	//	login(_w, _r)
-	indexTemplate.Execute(_w, nil)
+func handleTopstories(w http.ResponseWriter, r *http.Request) {
+	handle(w, r, GET_TOP_STORIES)
 }
 
-func loadClients(_r *http.Request) (clients []OtherClient) {
-	cxt := appengine.NewContext(_r)
-	q := datastore.NewQuery("OtherClient")
-	clients = make([]OtherClient, 0)
-	q.GetAll(cxt, &clients)
-	return
+func handleNewstories(w http.ResponseWriter, r *http.Request) {
+	handle(w, r, GET_NEWS_STORIES)
 }
 
-func handleResponse(_w http.ResponseWriter, _r *http.Request) {
+func handleAskstories(w http.ResponseWriter, r *http.Request) {
+	handle(w, r, GET_ASK_STORIES)
+}
+
+func handleShowstories(w http.ResponseWriter, r *http.Request) {
+	handle(w, r, GET_SHOW_STORIES)
+}
+
+func handleJobstories(w http.ResponseWriter, r *http.Request) {
+	handle(w, r, GET_JOB_STORIES)
+}
+
+//Handle different request on different APIs.
+func handle(w http.ResponseWriter, r *http.Request, api string) {
 	defer func() {
 		if err := recover(); err != nil {
-			cxt := appengine.NewContext(_r)
-			cxt.Errorf("handleResponse: %v", err)
+			cxt := appengine.NewContext(r)
+			cxt.Errorf("handle: %v", err)
 		}
 	}()
-	clients := loadClients(_r)
-	push(_w, _r, clients, false)
-	responseTemplate.Execute(_w, fmt.Sprintf("Finished client push users:%d", len(clients)))
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-//For scheduled tasks.
-////////////////////////////////////////////////////////////////////////////////////////////////////
-func handleResponseX(_w http.ResponseWriter, _r *http.Request) {
-	defer func() {
-		if err := recover(); err != nil {
-			cxt := appengine.NewContext(_r)
-			cxt.Errorf("handleResponseX: %v", err)
-		}
-	}()
-	clients := loadClients(_r)
-	push(_w, _r, clients, true)
-	responseTemplate.Execute(_w, fmt.Sprintf("Finished client push users:%d", len(clients)))
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-func handleDeleteAllUsers(_w http.ResponseWriter, _r *http.Request) {
-	cxt := appengine.NewContext(_r)
-	q := datastore.NewQuery("OtherClient")
-	clients := make([]OtherClient, 0)
-	keys, _ := q.GetAll(cxt, &clients)
-	datastore.DeleteMulti(cxt, keys)
-
-	//prints all data after deleting
-	keys, _ = q.GetAll(cxt, &clients)
-	fmt.Fprintf(_w, "rest:%v", len(keys))
-}
-
-func handleInsert(_w http.ResponseWriter, _r *http.Request) {
-	defer func() {
-		if err := recover(); err != nil {
-			status(_w, false, "insert")
-		}
-	}()
-	cxt := appengine.NewContext(_r)
-	cookies := _r.Cookies()
-
-	q := datastore.NewQuery("OtherClient").Filter("Account =", cookies[0].Value)
-	clients := make([]OtherClient, 0)
-	keys, _ := q.GetAll(cxt, &clients)
-	if len(clients) > 0 {
-		//Delete old one if find a existed item.
-		datastore.DeleteMulti(cxt, keys)
-	}
-
-	isFullText, _ := strconv.ParseBool(cookies[2].Value)
-	msgCount, _ := strconv.Atoi(cookies[3].Value)
-	allowEmptyUrl, _ := strconv.ParseBool(cookies[4].Value)
-	otherClient := &OtherClient{cookies[0].Value, cookies[1].Value, isFullText, msgCount, allowEmptyUrl}
-	datastore.Put(cxt, datastore.NewIncompleteKey(cxt, "OtherClient", nil), otherClient)
-	status(_w, true, "insert")
-
-	//Init push
-	clients = []OtherClient{*otherClient}
-	push(_w, _r, clients, false)
-}
-
-func handleDelete(_w http.ResponseWriter, _r *http.Request) {
-	defer func() {
-		if err := recover(); err != nil {
-			status(_w, false, "delete")
-		}
-	}()
-	cxt := appengine.NewContext(_r)
-	cookies := _r.Cookies()
-	q := datastore.NewQuery("OtherClient").Filter("Account =", cookies[0].Value)
-	clients := make([]OtherClient, 0)
-	keys, _ := q.GetAll(cxt, &clients)
-	datastore.DeleteMulti(cxt, keys)
-	status(_w, true, "delete")
-}
-
-func handleEdit(_w http.ResponseWriter, _r *http.Request) {
-	defer func() {
-		if err := recover(); err != nil {
-			status(_w, false, "edit")
-		}
-	}()
-	cxt := appengine.NewContext(_r)
-	cookies := _r.Cookies()
-	q := datastore.NewQuery("OtherClient").Filter("Account =", cookies[0].Value)
-	clients := make([]OtherClient, 0)
-	keys, _ := q.GetAll(cxt, &clients)
-	isFullText, _ := strconv.ParseBool(cookies[2].Value)
-	msgCount, _ := strconv.Atoi(cookies[3].Value)
-	allowEmptyUrl, _ := strconv.ParseBool(cookies[4].Value)
-	otherClient := &OtherClient{cookies[0].Value, cookies[1].Value, isFullText, msgCount, allowEmptyUrl}
-	datastore.Put(cxt, keys[0], otherClient)
-	status(_w, true, "edit")
+	push(w, r, api)
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 //For sync request.
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-func handleSync(_w http.ResponseWriter, _r *http.Request) {
-	// defer func() {
-	//   if err := recover(); err != nil {
-	//     fmt.Fprintf(_w, "Some error happened might: sync")
-	//   }
-	// }()
-	cxt := appengine.NewContext(_r)
-	cookies := _r.Cookies()
-	q := datastore.NewQuery("OtherClient").Filter("Account =", cookies[0].Value)
-	clients := make([]OtherClient, 0)
-	q.GetAll(cxt, &clients)
-	client := clients[0]
-	sync(_w, _r, &client)
+func handleSync(w http.ResponseWriter, r *http.Request) {
+	sync(w, r)
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-
-func status(_w http.ResponseWriter, ok bool, funcName string) {
+func status(w http.ResponseWriter, ok bool, funcName string) {
 	status := "false"
 	if ok {
 		status = "true"
 	}
 	s := fmt.Sprintf(`{"status":%s, "function":"%s" }`, status, funcName)
-	_w.Header().Set("Content-Type", API_RESTYPE)
-	fmt.Fprintf(_w, s)
+	w.Header().Set("Content-Type", API_RESTYPE)
+	fmt.Fprintf(w, s)
 }
